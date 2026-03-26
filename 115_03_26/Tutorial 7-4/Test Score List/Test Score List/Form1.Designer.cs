c0ba4e54078f707bbaa4dc45db46f133991ecad8
陳則愷 <a114221094@mail.shu.edu.tw>
2026-03-26 16:01:14 +0800

    115/03/26

---
+++ 115_03_26/Tutorial 7-4/Test Score List/Test Score List/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Test_Score_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testScoresListBox = new System.Windows.Forms.ListBox();
+            this.avgScoreDescriptionLabel = new System.Windows.Forms.Label();
+            this.aboveAvgDescriptionLabel = new System.Windows.Forms.Label();
+            this.belowAvgDescriptionLabel = new System.Windows.Forms.Label();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.aboveAverageLabel = new System.Windows.Forms.Label();
+            this.belowAverageLabel = new System.Windows.Forms.Label();
+            this.getScoresButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            // 新增搜尋元件
+            this.searchScoreLabel = new System.Windows.Forms.Label();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.searchResultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testScoresListBox
+            // 
+            // 列表框，用於顯示所有考試分數
+            this.testScoresListBox.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.testScoresListBox.FormattingEnabled = true;
+            this.testScoresListBox.ItemHeight = 24;
+            this.testScoresListBox.Location = new System.Drawing.Point(20, 20);
+            this.testScoresListBox.Name = "testScoresListBox";
+            this.testScoresListBox.Size = new System.Drawing.Size(200, 244);
+            this.testScoresListBox.TabIndex = 0;
+            // 
+            // avgScoreDescriptionLabel
+            // 
+            // 標籤，顯示「平均分數：」說明文字
+            this.avgScoreDescriptionLabel.AutoSize = true;
+            this.avgScoreDescriptionLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.avgScoreDescriptionLabel.Location = new System.Drawing.Point(380, 40);
+            this.avgScoreDescriptionLabel.Name = "avgScoreDescriptionLabel";
+            this.avgScoreDescriptionLabel.Size = new System.Drawing.Size(130, 24);
+            this.avgScoreDescriptionLabel.TabIndex = 1;
+            this.avgScoreDescriptionLabel.Text = "平均分數：";
+            // 
+            // aboveAvgDescriptionLabel
+            // 
+            // 標籤，顯示「高於平均分數的數量：」說明文字
+            this.aboveAvgDescriptionLabel.AutoSize = true;
+            this.aboveAvgDescriptionLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.aboveAvgDescriptionLabel.Location = new System.Drawing.Point(236, 110);
+            this.aboveAvgDescriptionLabel.Name = "aboveAvgDescriptionLabel";
+            this.aboveAvgDescriptionLabel.Size = new System.Drawing.Size(274, 24);
+            this.aboveAvgDescriptionLabel.TabIndex = 2;
+            this.aboveAvgDescriptionLabel.Text = "高於平均分數的數量：";
+            // 
+            // belowAvgDescriptionLabel
+            // 
+            // 標籤，顯示「低於平均分數的數量：」說明文字
+            this.belowAvgDescriptionLabel.AutoSize = true;
+            this.belowAvgDescriptionLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.belowAvgDescriptionLabel.Location = new System.Drawing.Point(236, 180);
+            this.belowAvgDescriptionLabel.Name = "belowAvgDescriptionLabel";
+            this.belowAvgDescriptionLabel.Size = new System.Drawing.Size(274, 24);
+            this.belowAvgDescriptionLabel.TabIndex = 3;
+            this.belowAvgDescriptionLabel.Text = "低於平均分數的數量：";
+            // 
+            // averageLabel
+            // 
+            // 標籤，用於顯示計算出的平均分數結果
+            this.averageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.averageLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.averageLabel.Location = new System.Drawing.Point(520, 35);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(150, 35);
+            this.averageLabel.TabIndex = 4;
+            this.averageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // aboveAverageLabel
+            // 
+            // 標籤，用於顯示高於平均的分數數量結果
+            this.aboveAverageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.aboveAverageLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.aboveAverageLabel.Location = new System.Drawing.Point(520, 105);
+            this.aboveAverageLabel.Name = "aboveAverageLabel";
+            this.aboveAverageLabel.Size = new System.Drawing.Size(150, 35);
+            this.aboveAverageLabel.TabIndex = 5;
+            this.aboveAverageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // belowAverageLabel
+            // 
+            // 標籤，用於顯示低於平均的分數數量結果
+            this.belowAverageLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.belowAverageLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.belowAverageLabel.Location = new System.Drawing.Point(520, 175);
+            this.belowAverageLabel.Name = "belowAverageLabel";
+            this.belowAverageLabel.Size = new System.Drawing.Size(150, 35);
+            this.belowAverageLabel.TabIndex = 6;
+            this.belowAverageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // getScoresButton
+            // 
+            // 按鈕，點擊後會讀取分數並計算顯示結果
+            this.getScoresButton.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.getScoresButton.Location = new System.Drawing.Point(180, 420);
+            this.getScoresButton.Name = "getScoresButton";
+            this.getScoresButton.Size = new System.Drawing.Size(150, 50);
+            this.getScoresButton.TabIndex = 7;
+            this.getScoresButton.Text = "取得分數";
+            this.getScoresButton.UseVisualStyleBackColor = true;
+            this.getScoresButton.Click += new System.EventHandler(this.getScoresButton_Click);
+            // 
+            // exitButton
+            // 
+            // 按鈕，點擊後會關閉應用程式
+            this.exitButton.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.exitButton.Location = new System.Drawing.Point(370, 420);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(150, 50);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "離開";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // searchScoreLabel
+            // 
+            this.searchScoreLabel.AutoSize = true;
+            this.searchScoreLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.searchScoreLabel.Location = new System.Drawing.Point(236, 250);
+            this.searchScoreLabel.Name = "searchScoreLabel";
+            this.searchScoreLabel.Size = new System.Drawing.Size(130, 24);
+            this.searchScoreLabel.TabIndex = 9;
+            this.searchScoreLabel.Text = "搜尋成績：";
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.searchTextBox.Location = new System.Drawing.Point(384, 247);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(100, 36);
+            this.searchTextBox.TabIndex = 10;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.searchButton.Location = new System.Drawing.Point(520, 242);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(100, 45);
+            this.searchButton.TabIndex = 11;
+            this.searchButton.Text = "搜尋";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // searchResultLabel
+            // 
+            this.searchResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.searchResultLabel.Font = new System.Drawing.Font("PMingLiU", 18F);
+            this.searchResultLabel.Location = new System.Drawing.Point(240, 310);
+            this.searchResultLabel.Name = "searchResultLabel";
+            this.searchResultLabel.Size = new System.Drawing.Size(430, 70);
+            this.searchResultLabel.TabIndex = 12;
+            this.searchResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            // 主表單設定
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 500);
+            this.Controls.Add(this.searchResultLabel);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.searchScoreLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.getScoresButton);
+            this.Controls.Add(this.belowAverageLabel);
+            this.Controls.Add(this.aboveAverageLabel);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.belowAvgDescriptionLabel);
+            this.Controls.Add(this.aboveAvgDescriptionLabel);
+            this.Controls.Add(this.avgScoreDescriptionLabel);
+            this.Controls.Add(this.testScoresListBox);
+            this.Name = "Form1";
+            this.Text = "考試分數列表";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox testScoresListBox;
+        private System.Windows.Forms.Label avgScoreDescriptionLabel;
+        private System.Windows.Forms.Label aboveAvgDescriptionLabel;
+        private System.Windows.Forms.Label belowAvgDescriptionLabel;
+        private System.Windows.Forms.Label averageLabel;
+        private System.Windows.Forms.Label aboveAverageLabel;
+        private System.Windows.Forms.Label belowAverageLabel;
+        private System.Windows.Forms.Button getScoresButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label searchScoreLabel;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label searchResultLabel;
+    }
+}
+
---
+++ 115_03_26/Tutorial 7-4/Test Score List/Test Score List/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Test_Score_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadScores(List<int> scoresList)
+        {
+            string filePath = "TestScores.txt"; // 分數檔案的路徑
+            // 從檔案中讀取分數並存入 List 集合
+            try
+            {
+                using (StreamReader reader = File.OpenText(filePath))
+                {
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        scoresList.Add(int.Parse(reader.ReadLine()));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error reading scores: " + ex.Message);
+            }
+        }
+
+        // DisplayScores 方法用來在列表框中顯示所有分數
+        private void DisplayScores(List<int> scoresList)
+        {
+            int index = 1; // 用來顯示分數的序號
+            foreach (int score in scoresList)
+            {
+                testScoresListBox.Items.Add((index++) + "; " + score);
+            }
+        }
+
+        private void getScoresButton_Click(object sender, EventArgs e)
+        {
+            double averageScore;    // 用來儲存平均分數
+            int numAboveAverage;    // 用來儲存高於平均分數的數量
+            int numBelowAverage;    // 用來儲存低於平均分數的數量
+
+            // 建立一個 List 集合來儲存分數
+            List<int> scoresList = new List<int>();
+
+            // 從檔案中讀取分數並存入 List 集合
+            ReadScores(scoresList);
+
+            // 在列表框中顯示所有分數
+            DisplayScores(scoresList);
+
+            // 計算並顯示平均分數
+            averageScore = Average(scoresList);
+            averageLabel.Text = averageScore.ToString("n1");
+
+            // 計算並顯示高於平均分數的數量
+            numAboveAverage = AboveAverage(scoresList, averageScore);
+            aboveAverageLabel.Text = numAboveAverage.ToString();
+
+            // 計算並顯示低於平均分數的數量
+            numBelowAverage = BelowAverage(scoresList);
+            belowAverageLabel.Text = numBelowAverage.ToString();
+        }
+
+        //searchButton_Click 方法用來搜尋分數並顯示結果
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            int searchScore;   // 用來儲存使用者輸入的分數
+            int position;         // 用來儲存搜尋到的分數的索引
+            // 取得使用者輸入的分數
+            if (!int.TryParse(searchTextBox.Text, out searchScore))
+            {
+                searchResultLabel.Text = "Please enter a valid score.";
+            }
+
+            if ((position = testScoresListBox.IndexOf(searchScore)) != -1)
+            {
+                searchResultLabel.Text = "Score found at position: " + (position + 1);
+                return;
+            }
+
+            else
+            {
+               searchResultLabel.Text = "Score not found.";
+                return;
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
